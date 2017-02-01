f735f216f0310a73d21513f9e5cac2b40b03f35b
Joel Seligstein <joel@seligstein.com>
2017-02-01 00:17:18 +0000

    update generator to have token gen

--- tools/Generator.php
+++ tools/Generator.php
@@ -4,14 +4,39 @@ namespace jaf\tools;
 
 class Generator {
 
-    public function salt($length) {
-        $chars = 'abcdefghijklmnopqrstuvwxyz0123456789-_';
+    public static function randSecure($min, $max) {
+    	$range = $max - $min;
+    	if ($range < 1) {
+			return $min;
+		}
+
+    	$log = ceil(log($range, 2));
+    	$bytes = (int) ($log / 8) + 1; // length in bytes
+    	$bits = (int) $log + 1; // length in bits
+    	$filter = (int) (1 << $bits) - 1; // set all lower bits to 1
+    	do {
+        	$rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes)));
+        	$rnd = $rnd & $filter; // discard irrelevant bits
+    	} while ($rnd > $range);
+    	return $min + $rnd;
+	}
+
+
+    public static function token($length) {
+        $chars = 'abcdefghijklmnopqrstuvwxyz'
+			   . 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+			   . '0123456789';
         $charLen = strlen($chars);
         $result = '';
         while ($length > 0) {
-            $result .= $chars[rand(0, $charLen-1)];
+            $result .= $chars[self::randSecure(0, $charLen-1)];
         }
         return $result;
     }
+
+	// storing in case someone else uses it
+	public function salt($length) {
+		return Generator::token($length);
+	}
 }
             
